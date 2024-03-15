1e2e6cd745ca97950299e341589d437ecc73f1df
JunHuang <jhuang@simwell.io>
2024-03-15 17:44:01 -0400

    multi tenant

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,34 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 import axios from "axios";
 
+// personal app sso single-tenant
+// const clientId = "c909cf5b-0efb-4e96-9c3f-9a7610d12d1d";
+
+// personal app sso single-tenant 2
+// const clientId = "edd81949-9b05-4b47-8aa8-c060fcc2f062";
+
+// personal app sso multi-tenant 2
+// const clientId = "09f595df-9faa-4295-a1ab-17ac9422fc3d";
+
+// personal app sso multi-tenant 3
+// const clientId = "af6a4deb-d619-4fdf-b0d8-4e58b243af63";
+
+// simwell app multi-tenant
+const clientId = "031f035f-6ca6-4e1d-a0fc-f76c8ba2906f";
+
+// personal AD
+// const authority =
+//   "https://login.microsoftonline.com/30ffb211-3192-4dc7-98a1-990e9e8af9c2";
+
+// Common for multi-tenant: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/initialization.md#optional-configure-authority
+const authority = "https://login.microsoftonline.com/common/";
+
 const msalConfig = {
   // https://azuread.github.io/microsoft-authentication-library-for-js/ref/types/_azure_msal_browser.Configuration.html
   // https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
   auth: {
-    clientId: "a61d496c-f773-4e7a-99b6-01fcbd361df7",
-    authority:
-      "https://login.microsoftonline.com/30ffb211-3192-4dc7-98a1-990e9e8af9c2",
+    clientId: clientId,
+    authority: authority,
     redirectUri: "http://localhost:5173/",
   },
   cache: {
@@ -39,6 +60,9 @@ export default {
       auth_token: {},
       access_token: {},
       username: "No User",
+      info: undefined,
+      tothebacksucceed: false,
+      token_var_status: 0,
     };
   },
 
@@ -51,16 +75,22 @@ export default {
     getUInfoFromBackEnd() {
       console.log(this.access_token.accessToken);
       axios
-        .get("http://localhost:8080/auth/user", {
+        .get("http://localhost:8080/user", {
           headers: {
             // Authorization: "Bearer ".concat(this.auth_token.accessToken),
             Authorization: "Bearer ".concat(this.access_token.idToken),
           },
         })
-        .then((response) => (this.info = response));
+        .then((response) => {
+          this.info = response.data;
+          this.token_var_status = response.status;
+        })
+        .catch((e) => {
+          console.log(e);
+          this.token_var_status = e.response.status;
+        });
     },
     async login() {
-      console.log(123);
       await msalInstance.loginPopup(loginRequest).then((tokenResponse) => {
         console.log(tokenResponse);
         this.auth_token = tokenResponse;
@@ -76,12 +106,22 @@ export default {
       // 3. If we want to act like a fool, using the idToken to replace the access_token and send any request to the backend is also doable, but....
       // 4. Access token acquired out of the cases from the aboved will not be varified by the backend, some ideas can be found: https://authguidance.com/azure-ad-troubleshooting/
       // so this is necessary:
-      tokenRequest.scopes = ["a61d496c-f773-4e7a-99b6-01fcbd361df7/.default"];
+      // tokenRequest.scopes = [clientId + "/.default"];
+
+      // for multi tenant app,
+      // check also: https://www.youtube.com/watch?v=NyZz1ICG7dQ
+      // admin user can access without any scope
+
+      // home tenant user can use scope /.default or via the exposed api scoped
+      // tokenRequest.scopes = [clientId + "/.default"];
+
+      // but, in order to allow external personal user get the access token, we should exposed an api
+      // external persional user can only access via exposed api scoped
+      tokenRequest.scopes = ["api://" + clientId + "/token"];
 
       await msalInstance
         .acquireTokenPopup(tokenRequest)
         .then((tokenResponse) => {
-          console.log(3321);
           console.log(tokenResponse);
           this.access_token = tokenResponse;
         });
@@ -126,11 +166,26 @@ export default {
     <!-- </div> -->
     <br />
     <a @click="getUInfoFromBackEnd">To back End</a>
+    <span> Backend token verification status: {{ token_var_status }}</span>
     <br />
     <br />
-    <div id="uinfo" style="width: 500px">
-      {{ JSON.stringify(auth_token.account, "\r\n", 4) }}
-    </div>
+    <hr />
+    <br />
+    <pre id="token" style="width: 500px">
+      Token Account:
+      <br />
+      {{ JSON.stringify(auth_token.account, undefined, 2) }}
+    </pre>
+    <br />
+
+    <hr />
+    <br />
+    <br />
+    <pre id="uinfo" style="width: 500px">
+      U Info From Backend:
+      <br />
+      {{ JSON.stringify(access_token.account, undefined, 2) }}
+    </pre>
   </div>
 
   <!-- <RouterView /> -->
